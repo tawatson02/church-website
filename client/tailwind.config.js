a423efbec30eccbf470a2c8219518f00c4b25607
Tim Watson <ta_watson@live.com>
2024-08-14 16:03:01 -0500

    Setup remaining necessities for Tailwind.

---
+++ client/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/**/*.jsx",
+      "./src/**/*.jsx",
+      "./src/*.jsx",
+      "*",
+    ],
+    theme: {
+      colors: {
+        darkerBlue: '#0071BA',
+        lightBlue: '#1DA0DB',
+        paleGreen: '#00A88B',
+        gunmetal: '#2A3030',
+        outerSpace: '#454C4C',
+        whiteSmoke: '#F5F5F5',
+        white: '#FBFCFF',
+        },
+      extend: {
+      },
+    },
+    plugins: [],
+  }
+  
+  
