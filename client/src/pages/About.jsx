be41900d6a3d8f7dd2d7ffeaab3cbf988e6333f7
Tim Watson <ta_watson@live.com>
2024-09-16 13:43:59 -0500

    changed the server.js by taking out the unused apolloserver

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -59,40 +59,59 @@ function AboutUs() {
               Assemblies of God Fundamental Truths
             </p>
             
-<h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">WE BELIEVE...</h2>
-<ul class="max-w-md space-y-1 text-gray-500 list-none list-inside dark:text-gray-400">
+<h2 class="mb-2 text-xl font-semibold text-gray-900 dark:text-white">WE BELIEVE...</h2>
+<ul class="max-w-md space-y-1 text-black list-none list-inside dark:text-gray-400">
     <li>
-        The Scriptures are inspired by God and declare his design and plan for mankind.
+    - WE BELIEVE... The Scriptures are inspired by God and declare his design and plan for mankind.
     </li>
     <li>
-        There is only One True God - revealed in three persons.. Father, Son, and Holy Spirit (commonly know as the Trinity).
+    - WE BELIEVE... There is only One True God - revealed in three persons.. Father, Son, and Holy Spirit (commonly know as the Trinity).
     </li>
     <li>
-        In the Deity of the Lord Jesus Christ. As God's son Jesus was both human and divine.
+    - WE BELIEVE... In the Deity of the Lord Jesus Christ. As God's son Jesus was both human and divine.
     </li>
     <li>
-        Though originally good, Man Willingly fell into Sin - ushering evil and death, both physical and spiritual, into the world.
+    - WE BELIEVE... Though originally good, Man Willingly fell into Sin - ushering evil and death, both physical and spiritual, into the world.
     </li>
     <li>
-         Every Person Can Have Restored Fellowship with God Through 'Salvation'(trusting Christ through faith and repentance to be our personal Savior).
+    - WE BELIEVE... Every Person Can Have Restored Fellowship with God Through 'Salvation'(trusting Christ through faith and repentance to be our personal Savior).
     </li>
     <li>
-        We practice two ordinances (1) Water Baptism by Immersion after repenting of one's sins and recieving Christ's gift of salvation, and (2) Holy Communion (the Lord's Supper) as a syumbolic remembrance of Christ's suffering and death for our salvation.
+    - WE BELIEVE... We practice two ordinances (1) Water Baptism by Immersion after repenting of one's sins and recieving Christ's gift of salvation, and (2) Holy Communion (the Lord's Supper) as a 
+       syumbolic remembrance of Christ's suffering and death for our salvation.
     </li>
     <li>
-        
+    - WE BELIEVE... The Baptism in the Holy Spirit is a Special Experience Following Salvation that empowers believers for witnessing and effective service, just as it did in New Testament times.
     </li>
     <li>
-        
+    - WE BELIEVE... The Initial Physical Evidence of the Baptism in the Holy Spirit is Speaking in Tongues as experienced on the Day of Pentecost and referenced throughout Acts and the Epistles.
     </li>
     <li>
-        
+    - WE BELIEVE... Sanctification Initially Occurs at Salvation and is not a declaration that a believer is holy, but also a progressive lifelong process of separating from evil as believers continually draw closer and become more Christ-like.
     </li>
     <li>
-        
+    - WE BELIEVE... The Church has a Mission to seek and save all who are lost in sin. We believe 'the Church' is the body of Christ and consists of the people who, through time, 
+        have accepted God's offer of redemption through the sacrificial death of His son Jesus Christ.
     </li>
     <li>
-        
+    - WE BELIEVE... A Divinely called and Scripturally Ordained leadership Ministry Serves the Church. The Bible teaches that each of us inder leadership must commit ourselves to reach others for Christ, to worship Him with other believers,
+         to build up or edify 
+        the body of believers - the Church and to meet human need with ministries of love and compassion.
+    </li>
+    <li>
+    - WE BELIEVE... Divine Healing of the Sick is a Privilege for Christians Today and is provided for in Christ's attonement. 
+    </li>
+    <li>
+    - WE BELIEVE... In the blessed Hope - When Jesus Raptures His Church Prior to His Return to Earth, this future moment in time all believers who have died will rise from their graves and meet the Lord in the air.
+    </li>
+    <li>
+    - WE BELIEVE... In the 
+    </li>
+    <li>
+    - WE BELIEVE...
+    </li>
+    <li>
+    - WE BELIEVE...
     </li>
 </ul>
 
