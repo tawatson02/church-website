6cc108a66391a6f42c721b84fab657db80d9f6b4
Tim Watson <ta_watson@live.com>
2024-09-16 14:15:51 -0500

    added vite.config.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,18 @@
 import * as React from 'react';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom'; 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import 'flowbite';
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-
 function App() {
   return (
-    <ApolloProvider client={client}>
-        <div className="flex flex-col justify-start min-h-screen">
-          <Header />
-          <div>
-            <Outlet />
-          </div>
-          <Footer />
-        </div>
-    </ApolloProvider>
+    <div className="flex flex-col justify-start min-h-screen">
+      <Header />
+      <div>
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
   );
 }
 
